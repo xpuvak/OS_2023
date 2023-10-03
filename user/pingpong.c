11db8dbf339155cacf2a5614ebd99aabb6c89b59
xpuvak <xpuvak@stuba.sk>
2023-10-03 19:37:05 +0200

    feat: edit pingpong

--- user/pingpong.c
+++ user/pingpong.c
@@ -5,26 +5,27 @@
 int
 main(int argc, char *argv[])
 {
- int to_parent[2], to_child[2];
+  int to_parent[2];
+  int to_child[2];
 
- pipe(to_parent);
- pipe(to_child);
+  pipe(to_parent);
+  pipe(to_child);
 
- int pid = fork();
+  int pid = fork();
 
- if(pid == 0){
-  // child
-  char received;
-  read(to_child[0], &received, 1);
-  printf("%d: received ping\n", getpid());
-  write(to_parent[1], "y", 1);
- } else{
-  // parent
-  write(to_child[1], "x", 1);
-  char received;
-  read(to_parent[0], &received, 1);
-  printf("%d: received pong\n", getpid());
- }
-
- exit(0);
+  if(pid == 0){
+    //child
+    char received;
+    read(to_child[0], &received, 1);
+    printf("%d: received ping\n", getpid());
+    write(to_parent[1], "y", 1);
+  }
+  else{
+    //parent
+    char received;
+    write(to_child[1], "x", 1);
+    read(to_parent[0], &received, 1);
+    printf("%d: received pong\n", getpid());
+  }
+  exit(0);
 }
