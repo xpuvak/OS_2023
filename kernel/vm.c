e8a01dd69cfe19de7ce2a2f0fdc125753d5d9d5f
xpuvak <xpuvak@stuba.sk>
2023-11-07 15:18:09 +0100

    feat: implement vmprint

--- kernel/vm.c
+++ kernel/vm.c
@@ -449,3 +449,31 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+// Recursively display page-table content.
+static int pagedeep = 0;
+	
+void
+vmprint(pagetable_t pagetable)
+{
+  if(pagedeep == 0)	
+    printf("page table %p\n", (uint64)pagetable);  
+  
+  // there are 2^9 = 512 PTEs in a page table.
+  for(int i = 0; i < 512; i++){
+    pte_t pte = pagetable[i];
+    if(pte & PTE_V){
+      for(int j=0; j<=pagedeep; j++)
+ 	printf(".. ");
+      printf("%d: pte %p pa %p\n", i, (uint64)pte, (uint64)PTE2PA(pte));   
+    }
+    if((pte & PTE_V) && (pte & (PTE_R|PTE_W|PTE_X)) == 0){
+      // this PTE points to a lower-level page table.
+      uint64 child = PTE2PA(pte);
+      pagedeep++;
+      vmprint((pagetable_t)child);
+      pagedeep--;
+    }  
+  }
+}
+
